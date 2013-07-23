cc5494cdfa7e5c8d4c3501f44a6b45a2cf4537ab
Tyler Gaw <tylerjgaw@gmail.com>
2013-07-23 18:34:08 -0400

    fixed pingpong threading

--- Threading/pingpong.cpp
+++ Threading/pingpong.cpp
@@ -2,25 +2,6 @@
 #include <boost/date_time.hpp>
 #include <boost/bind.hpp>
 
-//class Ball {
-//  boost::mutex mutex_;
-
-//public:
-  //void ping() {
-    //mutex_.lock();
-    //printf("%s\n", "Ping!");
-    //mutex_.unlock();
-    //return 0;
-  //}
-
-  //void pong() {
-   // mutex_.lock();
-    //printf("%s\n", "Pong!");
-    //mutex_.unlock();
-    //return 0;
-  //}
-//};
-
 void ping() {
   printf("%s\n", "Ping!");
 }
@@ -30,18 +11,12 @@ void pong() {
 }
 
 int main(int argc, char* argv[]) {
-  //boost::thread_group tgroup;
-  //Ball ball();
-  //tgroup.create_thread(boost::bind(&Ball::ping, &ball));
-  //tgroup.create_thread(boost::bind(&Ball::pong, &ball));
-
   printf("%s\n", "Ready... Set.. Go!");
   boost::thread pingt(ping);
   boost::thread pongt(pong);
   
   pingt.join();
   pongt.join();
-  //tgroup.join_all();
 
   printf("%s\n", "Done!");
   return 0;
